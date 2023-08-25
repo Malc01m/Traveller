21fb0f9310f181808736b963d7be29215fd3e4f6
Malcolm Worrell <malcolm.worrell.705@gmail.com>
2023-08-24 20:45:47 -0500

    randMultiple added

--- Math-tools/Rand/Rand.cpp
+++ Math-tools/Rand/Rand.cpp
@@ -8,3 +8,6 @@ float Rand::randFloatBetween(float min, float max) {
 int Rand::randIntBetween(int min, int max) {
     return (rand() % (max - min + 1)) + min;
 }
+int Rand::randMultiple(int base, int minFactor, int maxFactor) {
+    return base * Rand::randIntBetween(minFactor, maxFactor);
+}
--- Math-tools/Rand/Rand.h
+++ Math-tools/Rand/Rand.h
@@ -4,6 +4,7 @@
 namespace Rand {
     float randFloatBetween(float min, float max);
     int randIntBetween(int min, int max);
+    int randMultiple(int base, int minFactor, int maxFactor);
 }
 
 #endif
