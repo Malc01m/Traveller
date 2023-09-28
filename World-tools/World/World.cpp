c078f37a2da3ca6b98428532f616c22a080c96c2
Malcolm Worrell <malcolm.worrell.705@gmail.com>
2023-09-28 12:25:26 -0500

    Use of new tritiles in genterrain

--- World-tools/World/World.cpp
+++ World-tools/World/World.cpp
@@ -11,8 +11,10 @@ Group World::getWorldGroup() {
 }
 
 Group World::genTerrain() {
-    Group terrain = GeometryGen::triTiles(0.1, 5, 5);
-    terrain.setColor({0.2, 1, 0, 1});
-    terrain.scatterColorLightness(0.3);
+    Group terrain = GeometryGen::triTiles(0.1, 20, 20, 0.05);
+    terrain.centerAt({0, 0});
+    terrain.setColor({((float) 18 / 255), ((float) 212 / 255), ((float) 67 / 255), 1});
+    terrain.scatterColorLightness(0.06);
+    terrain.scale(2, 0);
     return terrain;
 }
